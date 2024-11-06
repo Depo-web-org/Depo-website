07b7197bea08fb310c2cac17cbc97841f915b2f5
osamamohamed88 <osama.mohamed@depowebeg.com>
2024-11-06 16:40:29 +0200

    navBar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+
+const Navbar = () => {
+  return (
+    <div className="bg-white h-[85px] fixed w-full ">
+        <div className=" ">
+            
+        </div>
+       
+    </div>
+  )
+}
+
+export default Navbar
