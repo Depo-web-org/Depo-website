aeb3998a56929c2105ab7a46a356b3921de76313
osamamohamed88 <osama.mohamed@depowebeg.com>
2024-11-06 16:53:02 +0200

    navWidth

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,16 @@
 const Navbar = () => {
   return (
     <div className="bg-white h-[85px] fixed w-full ">
-        <div className=" ">
-            
+        <div className="bg-secondary h-full  w-[calc(100%-104px)] mx-auto flex justify-between px-2 py-4   ">
+            <div className="h-full bg-red-500 w-1/4 ">
+
+            </div> 
+            <div className="h-full bg-red-600 w-1/2">
+
+            </div>
+            <div className="h-full bg-red-700 w-1/4">
+
+            </div>
         </div>
        
     </div>
