06950be0b407a3d8996143f19b25135aa4b8cf66
omargad-depoeg-99 <omar.gad@depowebeg.com>
2024-11-11 16:21:18 +0200

    enhancing responsive design

--- src/pages/Career/components/CareerContainer.jsx
+++ src/pages/Career/components/CareerContainer.jsx
@@ -4,7 +4,7 @@ export default function CareerContainer({ careerArray }) {
   console.log(careerArray);
   return (
     <>
-      <p className="font-[Inter] text-[32px] font-bold leading-[38.73px] text-left text-white ">
+      <p className="font-[Inter] text-[32px] pt-20 lg:pt-0  font-bold leading-[38.73px] text-left text-white ">
         Explore Available Opportunities
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-[40px]">
