1aa8998ceaf44e4c2dad454d791097776933b9ed
omargad-depoeg-99 <omar.gad@depowebeg.com>
2024-11-11 13:46:15 +0200

    style:fixing resposivity in services section

--- src/pages/Career/components/CareerContainer.jsx
+++ src/pages/Career/components/CareerContainer.jsx
@@ -3,15 +3,20 @@ import CareerCard from "./CareerCard";
 export default function CareerContainer({ careerArray }) {
   console.log(careerArray);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-4">
-      {careerArray.map((card, index) => (
-        <CareerCard
-          key={index}
-          index={index}
-          title={card.title}
-          subtitle={card.subtitle}
-        />
-      ))}
-    </div>
+    <>
+      <p className="font-[Inter] text-[32px] font-bold leading-[38.73px] text-left text-white ">
+        Explore Available Opportunities
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-[40px]">
+        {careerArray.map((card, index) => (
+          <CareerCard
+            key={index}
+            index={index}
+            title={card.title}
+            subtitle={card.subtitle}
+          />
+        ))}
+      </div>
+    </>
   );
 }
