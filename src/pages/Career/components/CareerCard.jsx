12d81f57c8f6c38e15dd271f5169e0bce2787fdc
omargad-depoeg-99 <omar.gad@depowebeg.com>
2024-11-11 15:31:20 +0200

    feat:adding career modal

--- src/pages/Career/components/CareerCard.jsx
+++ src/pages/Career/components/CareerCard.jsx
@@ -1,32 +1,45 @@
-export default function CareerCard({ title, subtitle, index }) {
-  console.log(title, subtitle);
-  return (
-    <div className="  h-[406px] p-2  gap-2 rounded-[16px]   bg-white shadow-lg">
-      {/* Main Content */}
+import { useState } from "react";
+import CareerModal from "./CareerModal";
 
+export default function CareerCard({ title, subtitle, index }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalToggle = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <div className="h-[406px] p-2 gap-2 rounded-[16px] bg-white shadow-lg">
+      {/* Main Content */}
       <div
-        className={`h-[332px] gap-0 rounded-t-[12px] rounded-br-none rounded-bl-none  ${
-          index % 2 === 0 ? "bg-secondary" : "bg-[#1A4F92B0] "
+        className={`h-[332px] gap-0 rounded-t-[12px] rounded-br-none rounded-bl-none ${
+          index % 2 === 0 ? "bg-secondary" : "bg-[#1A4F92B0]"
         }`}
       >
-        <div className="h-[88px] w-[173px] text-white pt-[64px] pl-[16px] ">
-          <p className="text-[36px] font-normal leading-[43.57px] text-left ">
+        <div className="h-[88px] w-[173px] text-white pt-[64px] pl-[16px]">
+          <p className="text-[36px] font-normal leading-[43.57px] text-left">
             {title}
           </p>
         </div>
       </div>
       {/* Footer */}
-      <div className="w-full h-[62px] flex justify-between items-center  bg-white">
+      <div className="w-full h-[62px] flex justify-between items-center bg-white">
         <p>{subtitle}</p>
-        <ViewButton />
+        <ViewButton onClick={handleModalToggle} />
       </div>
+
+      {/* Modal */}
+      {isModalOpen && <CareerModal onClose={handleModalToggle} />}
     </div>
   );
 }
 
-function ViewButton() {
+function ViewButton({ onClick }) {
   return (
-    <button className="w-[90px] h-[45px] px-8 py-4 gap-2 rounded-[23px] border text-center flex items-center justify-center border-secondary bg-white text-black">
+    <button
+      className="w-[90px] h-[45px] px-8 py-4 gap-2 rounded-[23px] border text-center flex items-center justify-center border-secondary bg-white text-black"
+      onClick={onClick}
+    >
       View
     </button>
   );
