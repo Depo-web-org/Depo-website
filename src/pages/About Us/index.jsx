dc275797cfd58147a40587308586d0717110b780
fawzybas <fawzy.basuony@depowebeg.com>
2024-11-12 10:04:18 +0200

    Helmet

--- src/pages/About Us/index.jsx
+++ src/pages/About Us/index.jsx
@@ -1,15 +1,26 @@
+import { Helmet } from "react-helmet-async";
 import PagesBg from "../../components/common/PagesBg";
 import About_Depo from "./components/About Depo/About_Depo";
 import Card_AboutUs from "./components/Card About/Card";
 
 export default function AboutUs() {
   return (
-    <div className="gradient-bg min-h-screen">
-      <PagesBg title="Get to know us better" />
-      <section className="w-[calc(100%-32px)] lg:w-[calc(100%-208px)]  mx-auto">
-        <About_Depo />
-        <Card_AboutUs />
-      </section>
-    </div>
+    <>
+      <Helmet>
+        <title>About Us</title>
+        <meta
+          name="description"
+          content="Learn about Depo web's journey, vision, and extensive experience in software development. We are dedicated to delivering the best solutions to help you achieve your goals."
+        />
+      </Helmet>
+
+      <div className="gradient-bg min-h-screen">
+        <PagesBg title="Get to know us better" />
+        <section className="w-[calc(100%-32px)] lg:w-[calc(100%-208px)]  mx-auto">
+          <About_Depo />
+          <Card_AboutUs />
+        </section>
+      </div>
+    </>
   );
 }
