80adb6dbb51180a6ac600e28c9082467b8afbc0f
omargad-depoeg-99 <omar.gad@depowebeg.com>
2024-11-12 16:52:11 +0200

    style: increase text size in services summary

--- src/pages/Services/components/Summary.jsx
+++ src/pages/Services/components/Summary.jsx
@@ -4,7 +4,7 @@ export default function Summary() {
   return (
     <div className="flex items-center lg:pt-[32px] pb-[32px] md:pb-[16px] text-white container">
       <div className="w-[502px]  h-[154px] flex flex-col justify-start items-start ">
-        <p className="font-inter text-2xl font-bold leading-[38.73px] text-left">
+        <p className="font-inter text-2xl md:text-[32px] font-bold leading-[38.73px] text-left">
           Our Services
         </p>
         <p className="font-inter text-lg italic font-normal leading-[24.2px] text-left">
