b9dfac4eb48464ff6dbb3604caa255f64613d7ac
omargad-depoeg-99 <omar.gad@depowebeg.com>
2024-11-12 09:44:16 +0200

    adding responsive design to nocareer component

--- src/pages/Career/components/NoCareer.jsx
+++ src/pages/Career/components/NoCareer.jsx
@@ -2,11 +2,11 @@ export default function NoCareer() {
   return (
     <div className="py-44  lg:py-0">
       <div>
-        <p className="font-[Inter] md:pb-24   text-[16px] md:text-[32px] font-bold leading-[38.73px] text-left text-white">
+        <p className="font-[Inter] pb-10   text-[16px] md:text-[32px] font-bold leading-[38.73px] text-left text-white">
           There are no jobs available right now
         </p>
       </div>
-      <div className="flex items-center py-28 lg:py-0 md:pt-0 justify-center -mt-[50px] relative z-20">
+      <div className="flex items-center  md:pt-0 justify-center -mt-[50px] relative z-20">
         <img
           src="public/images/career/Feeling sorry-bro.svg"
           className="w-2/3"
