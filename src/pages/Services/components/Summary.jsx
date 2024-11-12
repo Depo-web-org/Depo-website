af5cc591bb5420376af0544557da39657507ca64
omargad-depoeg-99 <omar.gad@depowebeg.com>
2024-11-12 14:18:07 +0200

    style:adding style to p summary

--- src/pages/Services/components/Summary.jsx
+++ src/pages/Services/components/Summary.jsx
@@ -4,8 +4,10 @@ export default function Summary() {
   return (
     <div className="flex items-center pt-[32px] text-white container">
       <div className="w-[502px]  h-[154px] flex flex-col justify-start items-start ">
-        <h1 className="font-semibold text-xl">Our Services</h1>
-        <p>
+        <p className="font-inter text-2xl font-bold leading-[38.73px] text-left">
+          Our Services
+        </p>
+        <p className="font-inter text-lg italic font-normal leading-[24.2px] text-left">
           we specialize in delivering high-quality digital solutions tailored to
           meet modern business needs.
         </p>
