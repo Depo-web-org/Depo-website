4b83f11c5df7097bd34b2ee5a159aac4aec55563
omargad-depoeg-99 <omar.gad@depowebeg.com>
2024-11-12 13:23:33 +0200

    adding resposivity to carousel and no career

--- src/pages/Career/components/NoCareer.jsx
+++ src/pages/Career/components/NoCareer.jsx
@@ -1,6 +1,6 @@
 export default function NoCareer() {
   return (
-    <div className="py-44  lg:py-0">
+    <div className="py-24  lg:py-0">
       <div>
         <p className="font-[Inter] pb-10   text-[16px] md:text-[32px] font-bold leading-[38.73px] text-left text-white">
           There are no jobs available right now
@@ -9,7 +9,7 @@ export default function NoCareer() {
       <div className="flex items-center  md:pt-0 justify-center -mt-[50px] relative z-20">
         <img
           src="public/images/career/Feeling sorry-bro.svg"
-          className="w-2/3"
+          className="w-full md:w-2/3"
           alt="no-career"
         />
       </div>
