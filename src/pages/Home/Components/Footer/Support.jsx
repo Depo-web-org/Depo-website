55cbc996348e11839d51fec7c8e58f98f0acb4fb
omargad-depoeg-99 <omar.gad@depowebeg.com>
2024-11-13 15:26:08 +0200

    adding pages links to footer

--- src/pages/Home/Components/Footer/Support.jsx
+++ src/pages/Home/Components/Footer/Support.jsx
@@ -1,5 +1,6 @@
 import { IoMdContact } from "react-icons/io";
 import { FaQuestionCircle, FaInfoCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Support() {
   return (
@@ -7,34 +8,34 @@ export default function Support() {
       <h3 className="font-semibold text-lg mb-4">Support</h3>
       <ul className="space-y-4">
         <li>
-          <a href="#" className="hover:text-teal-300">
+          <Link to="/about" className="hover:text-teal-300">
             <div className="flex items-start space-x-2">
               <div className="text-primary h-5 w-5">
                 <FaInfoCircle />
               </div>
               <span>About Us</span>
             </div>
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="#" className="hover:text-teal-300">
+          <Link to="/contact" className="hover:text-teal-300">
             <div className="flex items-start space-x-2">
               <div className="text-primary h-5 w-5">
                 <IoMdContact />
               </div>
               <span>Contact Us</span>
             </div>
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="#" className="hover:text-teal-300">
+          <Link to="#" className="hover:text-teal-300">
             <div className="flex items-start space-x-2">
               <div className="text-primary h-5 w-5">
                 <FaQuestionCircle />
               </div>
               <span>FAQs</span>
             </div>
-          </a>
+          </Link>
         </li>
       </ul>
     </div>
