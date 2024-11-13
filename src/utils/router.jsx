b13fc584c2c86f986eef6e2b0d40641a01d0823f
fawzybas <fawzy.basuony@depowebeg.com>
2024-11-13 14:14:07 +0200

    creat loading

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -1,22 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../pages/layout";
-import Home from "../pages/Home";
 import Contact from "../pages/Contact";
 import AboutUs from "../pages/About Us";
 import Services from "../pages/Services/Services";
 import Career from "../pages/Career/Career";
 import NotFound from "../pages/NotFound";
+import { lazy, Suspense } from "react";
+import Laoding from "../components/Laoding page/Laoding";
+const Home = lazy(() => import("../pages/Home"));
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      {path: '*', element: <NotFound/> },
+      { path: "*", element: <NotFound /> },
       {
         index: true,
-        element: <Home />,
-        // loader: locationMapLoader,
+        element: (
+          <Suspense fallback={<Laoding />}>
+            <Home />
+          </Suspense>
+        ),
       },
       {
         path: "contact",
