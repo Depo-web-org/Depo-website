f8789fe6a38f409e546923d38fff946a15c604bd
fawzybas <fawzy.basuony@depowebeg.com>
2024-11-14 10:17:20 +0200

    loading page

--- src/components/Laoding page/Laoding.jsx
+++ src/components/Laoding page/Laoding.jsx
@@ -2,8 +2,22 @@ import "./Laoding.css";
 export default function Laoding() {
   return (
     <>
-      <div className="gradient-bg min-h-screen flex justify-center items-center ">
-        <div className="loader "></div>
+      <div className="gradient-bg min-h-screen flex flex-col justify-center items-center">
+        <img
+          src="/images/lOGO.svg"
+          alt="loading img"
+          className="w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-40 object-contain "
+        />
+
+        {/* laoding */}
+        <div>
+          <div className="wrapper">
+            <div className="blue ball" />
+            <div className="red ball" />
+            <div className="yellow ball" />
+            <div className="green ball" />
+          </div>
+        </div>
       </div>
     </>
   );
